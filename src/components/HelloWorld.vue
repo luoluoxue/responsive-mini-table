ef6b1d4e8b21d057025f36906bf29361ce4239d2
lcx <473887719@qq.com>
2020-12-19 14:40:37 +0800

    vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="rows" v-for="(row, i) in renderData" :key="i">
+      <div class="cell" v-for="(td, j) in renderData[i]" :key="j">
+        <span style="color:red">{{ td.show(renderData) }}</span>
+        <input
+          type="checkbox"
+          @click="changeStatus(td)"
+          :checked="td.type == 'exp'"
+        />
+        <input type="text" v-model="td.input" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="./_index.ts" lang="ts"></script>
+<style>
+.rows {
+  width: 100%;
+  display: flex;
+}
+.cell {
+  width: 33.33%;
+  border: 1px solid #000;
+  height: 80px;
+  line-height: 80px;
+  margin: 0 5px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.css";
+// import VueCompositionAPI from "@vue/composition-api";
+createApp(App).mount("#app");
+// Vue.use(VueCompositionAPI);
